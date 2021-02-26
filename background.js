6b874b8e133e588d700fbafdc135ed3f56d6bf06
Kevin Lin <kevin@standard-deviancy.com>
2021-02-26 13:45:57 -0800

    Added popup and formatting

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 chrome.runtime.onInstalled.addListener(function(){
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: {hostEquals: 'www.reddit.com'},
-                })
+                pageUrl: {hostEquals: 'www.reddit.com'},
+            })
             ],
-            actions: [new chrome.declarativeContent.ShowPageAction()] 
+            actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
-    })
+    });
 });
---
+++ popup.js
@@ -0,0 +1,9 @@
+let autoForward = document.getElementById('autoforward')
+
+if (autoForward.checked = true) {
+    // change icon to red
+    // whenever on reddit.com, change to old.reddit.com.
+    // Does this mean we need to modify the network request?
+} else {
+    // change the icon to black 
+}
